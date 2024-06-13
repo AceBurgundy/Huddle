dfd6e2bc111f9b5438ce5e4ce907173a21d3ac17
AceBurgundy <samadriansabalo99@gmail.com>
2024-06-13 08:33:15 +0800

    Added script to update notifications.

--- Engine/index/static/index/socket.js
+++ Engine/index/static/index/socket.js
@@ -1,9 +1,21 @@
-import {socket, baseURL} from "../../../static/socket.js";
+import {allowedToFetch, isLoggedIn} from '../../../static/helper.js';
+import {socket} from '../../../static/socket.js';
+import {updateNotifications} from './notification.js';
 
 socket.on('connect', async () => {
-  console.log("socket started");
+  console.log('socket started');
+  const allowed = allowedToFetch();
+  const loggedIn = await isLoggedIn();
+
+  if (allowed && loggedIn) {
+    await updateNotifications();
+  }
 });
 
 socket.on('update_notification', async () => {
-  await updateNotifications();
+  const loggedIn = await isLoggedIn();
+
+  if (loggedIn) {
+    await updateNotifications();
+  }
 });
