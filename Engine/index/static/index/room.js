b4a9690836dc2251cdb9f3468b44eeeea226b98b
AceBurgundy <samadriansabalo99@gmail.com>
2024-06-13 08:24:20 +0800

    Added check if code is allowed to fetch.

--- Engine/index/static/index/room.js
+++ Engine/index/static/index/room.js
@@ -1,25 +1,27 @@
-import {element} from '../../../static/helper.js';
+import {allowedToFetch, element} from '../../../static/helper.js';
 
 const roomsContainer = element('#room-posts');
 const objectIsEmpty = object_ => JSON.stringify(object_) === '{}';
 
 try {
-  const rooms = await fetch('/room');
-  const response = await rooms.json();
-  const isEmpty = objectIsEmpty(response);
+  if (allowedToFetch()) {
+    const rooms = await fetch('/room');
+    const response = await rooms.json();
+    const isEmpty = objectIsEmpty(response);
 
-  const roomTemplate = (code, title, description) => /* html */ `
-    <a class="room-post" href="${code}">
-      <h2 class="room-post__title">${title}</h2>
-      <p class="room-post__description">${description}</p>
-    </a>
-  `;
+    const roomTemplate = (code, title, description) => /* html */ `
+      <a class="room-post" href="${code}">
+        <h2 class="room-post__title">${title}</h2>
+        <p class="room-post__description">${description}</p>
+      </a>
+    `;
 
-  if (!isEmpty) {
-    Object.entries(response).forEach(([code, {title, description}]) => {
-      if (!roomsContainer) return;
-      roomsContainer.innerHTML += roomTemplate(code, title, description);
-    });
+    if (!isEmpty) {
+      Object.entries(response).forEach(([code, {title, description}]) => {
+        if (!roomsContainer) return;
+        roomsContainer.innerHTML += roomTemplate(code, title, description);
+      });
+    }
   }
 } catch (error) {
   console.log(error);
