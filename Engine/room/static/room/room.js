7890da0fb9a4487da6160f8a4d745934d01c7d1c
AceBurgundy <samadriansabalo99@gmail.com>
2024-06-19 23:01:24 +0800

    Added js code for joining room.

--- Engine/room/static/room/room.js
+++ Engine/room/static/room/room.js
@@ -1,4 +1,25 @@
-// fetch("room/vzbielymx/data")
-//   .then(response => response.json())
-//   .then(data => console.log(data))
-//   .catch(error => console.error(error))
+import makeToastNotification, {isLoggedIn} from '../../../static/helper.js';
+import {socket} from '../../../static/socket.js';
+
+window.onload = async () => {
+  const loggedIn = await isLoggedIn();
+  const path = window.location.pathname.substring(1).split('/');
+  /**
+     * path must look something like this to proceed
+     * [room, <str: room_code>, open]
+     */
+  const inRoom = path[0] === 'room' && path[2] === 'open';
+
+  if (!loggedIn || !inRoom) {
+    return;
+  }
+
+  socket.emit('join', {
+    room: path[1]
+  });
+
+  socket.on('left', message => makeToastNotification(message));
+  socket.on('joined', message => makeToastNotification(message));
+};
+
+socket.on('message', message => makeToastNotification(message));
