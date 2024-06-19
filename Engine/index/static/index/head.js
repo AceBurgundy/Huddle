339557d9a97dfb0a05393a5f4d33b203665d4faf
AceBurgundy <samadriansabalo99@gmail.com>
2024-06-19 22:59:50 +0800

    Added code to hide alerts if not clicked.

--- Engine/index/static/index/head.js
+++ Engine/index/static/index/head.js
@@ -1,9 +1,22 @@
+import {element} from '../../../static/helper.js';
+
 window.onclick = event => {
   if (!event.target.id) return;
 
   const notToggledNotification = !['notification-toggle', 'notification-dark', 'notification-icon'].includes(event.target.id);
 
+  // Hides alerts list if not toggled
+  if (event.target.id !== 'main-content-rooms-chat-bottom-right-reminder') {
+    const alertsList = element('#main-content-rooms-chat-bottom-right-alerts');
+
+    if (alertsList) {
+      alertsList.classList.remove('active');
+    }
+  }
+
+  // hides notifications if not toggled
   if (notToggledNotification) {
-    document.getElementById('notification-list').classList.toggle('active');
+    const notificationList = element('#notification-list');
+    if (notificationList) notificationList.classList.remove('active');
   }
 };
